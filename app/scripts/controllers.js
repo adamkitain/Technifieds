7bbf0422d970c911d3683fb4ea85bc61f6a80f04
Adam Kitain <adamkitain@gmail.com>
2013-05-19 15:42:12 -0400

    added posting view

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -45,6 +45,31 @@ angular.module('technifieds.app').
             };
 
         }])
+    .controller('PostingCtrl', ['$scope', '$routeParams', 'Categories', '$location',
+    function($scope, $routeParams, Categories, $location){
+        $scope.viewPath = "/views/partials/posting.html";
+        $scope.routeParams = $routeParams;
+        $scope.classification = {};
+        $scope.selectedItem = {};
+
+        Categories.queryCategories().then(function(result){
+            $scope.categories = result.data;
+            $scope.classification.category = _.find($scope.categories, function(_category){
+                return $scope.routeParams.category === _category.path;
+            });
+            $scope.classification.subcategory = _.find($scope.classification.category.subcategories, function(_subcategory){
+                return $scope.routeParams.subcategory === _subcategory.path;
+            });
+        });
+
+        Categories.queryList($scope.routeParams.category, $scope.routeParams.subcategory).then(function(result){
+            $scope.list = result.data;
+            $scope.selectedItem = _.find($scope.list, function(item){
+                return $scope.routeParams.id == item._id;
+            });
+        });
+
+    }])
 
     .controller('AppCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
         $rootScope.$on("$routeChangeStart", function (event, next, current) {
